817cf9f11d00f952e3f6e45d543ffce4e21c4914
a4ru <earlvincentpilayre7@gmail.com>
2025-03-09 01:10:07 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hello, Kryztel! I have something'.split('')
+const text = 'Hello, Kryztel!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
